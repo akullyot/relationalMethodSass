56806dea3a6fa0003f7d63010a3ca38bda4c2dfa
Annie Ullyot <50632137+akullyot@users.noreply.github.com>
2024-01-22 01:42:11 -0600

    navbar and footer complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,9 @@
 import { useState }              from 'react'
 import { NavLink, Link}      from 'react-router-dom';
 //Required Media
-import logo from '../Assets/Images/Icons/relationMethodLogo.png'
+import logo from '../Assets/Images/Icons/relationMethodLogo.png';
+import hamburgerGraphicLeft from '../Assets/Images/readingHamburgerFamily.png';
+import hamburgerGraphicRight from '../Assets/Images/readingHamburgerWomen.png';
 
 export default function Navbar(){
   //State Variables
@@ -24,7 +26,9 @@ export default function Navbar(){
                 <Link to='/' className="homeLink">
                     <img id='navLogo' src={logo} alt="practice logo"/>                    
                 </Link>
-                <h4> Relational Method PLLC </h4>
+                <Link to='/' className="homeLink">
+                    <h4 id='navPracticeName'> Relational Method PLLC </h4>
+                </Link>
             </div>
             {
                 //only shows when the screen size is small enough width
@@ -35,11 +39,13 @@ export default function Navbar(){
               <div className="line3"></div>
             </div> 
             <ul className={navLinkOpen ? 'linkHolder open': 'linkHolder'}>
+                <img id='hamburgerGraphicRight' src={hamburgerGraphicRight} alt="womenReading" class={linksFading ? 'imageFading' : null}/>   
                 <li  className={linksFading ? 'fading': null} ><NavLink  className='navLink' to='/aboutme' >About Me</NavLink></li>
                 <li  className={linksFading ? 'fading': null} ><NavLink  className='navLink' to='/services' >Services</NavLink></li>
                 <li   className={linksFading ? 'fading': null}><NavLink  className='navLink' to='/location' >Location</NavLink></li>
                 <li  className={linksFading ? 'fading': null} ><NavLink  className='navLink' to='/insurance' >Insurance/Rates</NavLink></li>
                 <li  className={linksFading ? 'fading': null} ><NavLink className='navLink navButton' id='contact' to='/contact' >Contact</NavLink></li>
+                <img id='hamburgerGraphicLeft' src={hamburgerGraphicLeft} alt="familyReading" class={linksFading ? 'imageFading' : null}/>   
             </ul>
         </nav>
     )
