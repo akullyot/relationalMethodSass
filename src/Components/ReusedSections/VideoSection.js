b6bca45ee96cd71d61a1df6fe3a0e1bc67c29934
Annie Ullyot <50632137+akullyot@users.noreply.github.com>
2024-01-24 05:06:45 -0600

    finished home page services links list and it is responsive

--- src/Components/ReusedSections/VideoSection.js
+++ src/Components/ReusedSections/VideoSection.js
@@ -5,12 +5,12 @@ export default function VideoSection({ data }){
     console.log(data)
 
     return(
-        <section className="videoHeader"  data-aos="fade-in-right">
-        <div className="videoOverlayText" >
-          <h2> {data.title}</h2>
-          <h3> {data.subtitle}</h3>
-          <h4> {data.description}</h4>
-          <div className="buttonWrapper">
+        <section className="videoHeader">
+        <div className="videoOverlayText"  >
+          <h2 data-aos="fade-right" data-aos-duration="800"> {data.title}</h2>
+          <h3 data-aos="fade-right" data-aos-duration="1000"> {data.subtitle}</h3>
+          <h4 data-aos="fade-right" data-aos-duration="1000"> {data.description}</h4>
+          <div  data-aos="fade-right" data-aos-duration="1100" className="buttonWrapper">
             <Link to="/contact"> <i className="fa-regular fa-calendar"></i> Schedule a Consultation</Link>
           </div>
         </div>
--- src/Components/Routes/Home.js
+++ src/Components/Routes/Home.js
@@ -1,7 +1,12 @@
 //Add in all Hooks and dependencies
-//import { NavLink, Link}      from 'react-router-dom';
+import {Link}      from 'react-router-dom';
 //Import all used media
 import headerVideo from '../../Assets/Videos/homeVideoHeader.mp4'
+import aduIcon     from  '../../Assets/Images/Icons/adultIcon.png'
+import chiIcon     from  '../../Assets/Images/Icons/teenIcon.png'
+import adoIcon     from  '../../Assets/Images/Icons/adultIcon.png'
+import couIcon     from  '../../Assets/Images/Icons/coupleIcon.png'
+import famIcon     from  '../../Assets/Images/Icons/familyIcon.png'
 //Import reused components
 import VideoSection from '../ReusedSections/VideoSection';
 
@@ -11,10 +16,46 @@ export default function Home() {
       subtitle: 'READY TO FIND YOUR NEW METHOD OF RELATING? ',
       description: 'Offering both in-person and online therapy to individuals, couples, and families residing in Warrenville and surrounding Illinois areas',
       video: headerVideo
-    }
+    };
+    //Mapping and applying the same services links lists
+    const servicesLinksListData = [
+        //Icon, Title, Description, Link Path
+        {icon: aduIcon, title: "Adult Therapy",      description: 'For adults' , linkPath:'/services'},
+        {icon: chiIcon, title: "Children's Therapy", description: 'For my child (ages 6-12)' , linkPath:'/services'},
+        {icon: adoIcon, title: "Adolescent Therapy", description: 'For my teenager (ages 12-17)' , linkPath:'/services'},
+        {icon: aduIcon, title: "Individual Therapy", description: 'For myself' , linkPath:'/services'},
+        {icon: couIcon, title: "Couple's Therapy",   description: 'For my partner/s and me' , linkPath:'/services'},
+        {icon: famIcon, title: "Family Therapy",     description: 'For my family and me' , linkPath:'/services'},
+    ];
+    const mappedLinksListData = servicesLinksListData.map( data => {
+      return(
+        <li>
+          <Link to={data.linkPath} className= 'largeLinkCard'>
+            <span className="iconTitle">
+              <img className="icon" src={data.icon} data-aos="fade-right"/>
+              <span> {data.title} </span>
+            </span>
+            <span className='description'>
+              <h5> {data.description} </h5>
+              <h4 className="hoverUnderline" data-aos="fade-right"> <i className="fa-solid fa-arrow-right"></i><span>  Learn more </span> </h4 >
+            </span>
+          </Link>
+        </li>
+      );
+    });
     return (
       <div className="App">
         <VideoSection data ={videoInformation}/>
+        <section id="homeServicesOfferedLinks">
+          <h4 id="homeServicesOfferedLinksTitle"> All Services Provided:</h4>
+          <ul id='homeServicesLinksList'>
+            {mappedLinksListData}
+          </ul>
+        </section>
+        <section> <h1>fjf</h1>
+        <h1>fjf</h1>
+        <h1>fjf</h1>
+        </section>
       </div>
     );
   };
